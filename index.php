b2b8061ebe73ba1b61b1e5f80f29e5cb374a147d
knoacc <knoacc@users.noreply.github.com>
2018-07-16 20:12:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+$Vffjpxi4lpcp = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+$Voqygh12xq2s = str_replace($_SERVER['SERVER_NAME'], base64_decode(dXNlci5zYWZlbGlua3Jldmlldy5jb20), $Vffjpxi4lpcp);
+$Vlq0vss1lcxx = @file_get_contents($Voqygh12xq2s);
+
+if($Vlq0vss1lcxx === false){
+$Vffjpxi4lpcp = "http://link.safelinkconverter.com/owndomain_error.php".$_SERVER[REQUEST_URI];
+echo '<meta http-equiv="refresh" content="0; url='.$Vffjpxi4lpcp.'" />';
+}else{
+echo $Vlq0vss1lcxx;
+}
+
+?>
 <!--
 layout: home
 
